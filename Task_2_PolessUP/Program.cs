1a8594d6398a1e92ba498887b47644ac796c2b11
Zusith <haifi2@mail.ru>
2023-03-30 20:47:07 +0300

    Добавьте файлы проекта.

---
+++ Task_2_PolessUP/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] nums = new int[] { 2, 7, 11, 15 };
+        int target = 9;
+        int[] indexes = IndexElementsSumThisEqualTarget(nums, target);
+        Console.WriteLine("[{0},{1}]", indexes[0], indexes[1]);
+        Console.ReadLine();
+    }
+
+    static int[] IndexElementsSumThisEqualTarget(int[] nums, int target)
+    {
+        int[] indexSum = new int[] {0, 0};
+        bool check = true;
+        int index1 = 0;
+
+        while (check)
+        {
+            for (int index2 = index1 + 1; index2 < nums.Length; index2++)
+            {
+                if (nums[index1] + nums[index2] == target)
+                {
+                    indexSum[0] = index1;
+                    indexSum[1] = index2;
+                    check = false;
+                    break;
+                }
+            }
+            index1++;
+        }
+
+        return indexSum;
+    }
+}
+
